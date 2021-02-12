c3c7dd8f34c4f96a40e779b78584a866a5303957
Andrey Kislyak <deepdivenow@gmail.com>
2021-02-12 15:10:57 +0300

    Init #4

--- vars/StartEnv.groovy
+++ vars/StartEnv.groovy
@@ -4,12 +4,11 @@ def call (){
     echo "Services HelmChart ${env.SERVICES_HELM}:${env.SERVICES_HELM_VERSION}"
     env.SERVICES.tokenize(" ").each { s ->
         env.SERVICE_NAME=s
-        stage("Start ${SERVICE_NAME}") {
+            echo "Library Start ${SERVICE_NAME}"
             sh '''
                 [[ "X${SERVICE_NAME}" = "Xclickhouse" ]] && sed -i "s/ZOOKEEPER_HOST/zookeeper-${BUILD_HASH}-j/" ./${SERVICES_DIR}/clickhouse-values.yaml
                 helm install --atomic -f ./${SERVICES_DIR}/${SERVICE_NAME}-values.yaml ${SERVICE_NAME}-${BUILD_HASH} \
                   --set nameOverride="j" -n "jenkins" carrotquest/${SERVICES_HELM} --version ${SERVICES_HELM_VERSION}
             '''
-        }
     }
 }
--- vars/StopEnv.groovy
+++ vars/StopEnv.groovy
@@ -3,11 +3,10 @@ def call (){
     env.SERVICES.tokenize(" ").each { s ->
         env.SERVICE_NAME=s
         try{
-            stage("Stop ${SERVICE_NAME}") {
+            echo "Library Stop ${SERVICE_NAME}"
                 sh '''
                     helm uninstall ${SERVICE_NAME}-${BUILD_HASH} -n "jenkins"
                 '''
-            }
         } catch(e) {
             echo e.toString()
         }
